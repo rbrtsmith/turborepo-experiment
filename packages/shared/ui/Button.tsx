166448bcfcce9e74bde72f60bacd9d76896ffb46
Robert Smith <rbrtsmith84@googlemail.com>
2023-01-26 18:44:34 +0000

    add tsconfig package

--- packages/shared/ui/Button.tsx
+++ packages/shared/ui/Button.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
 
 export const Button = ({ children }: { children: React.ReactNode }) => {
-  return <button style={{ background: 'orange' }}>{children}</button>;
+  return <button style={{ background: 'blue' }}>{children}</button>;
 };
